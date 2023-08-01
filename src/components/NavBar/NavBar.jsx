89aef738e830143c47a21914fb16972abc31b430
joacodiler <jdilernia99@gmail.com>
2023-08-01 10:35:27 -0300

    creamos los componentes y el navbar con react router

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import './NavBar.css'
+
+
+const NavBar = () => {
+    const [menu, setMenu] = useState(false);
+
+    return (
+
+        <nav>
+            <Link to='/' className='titulo'> BAHALA </Link>
+            <div className='menu' onClick={() => { setMenu(!menu) }}>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul className={menu ? "open" : ""}>
+                <li>
+                    <NavLink to='/'>Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/servicios'>Servicios</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/contacto'>Contacto</NavLink>
+                </li>
+            </ul>
+        </nav>
+
+    )
+}
+
+export default NavBar
