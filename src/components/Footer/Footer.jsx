7ffba89ca4fd14760287950c9e2965079a818b4c
joacodiler <jdilernia99@gmail.com>
2023-08-01 16:43:59 -0300

    icono de ig y wpp con sus respectivos links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,9 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <footer>
-      <h1>footer</h1>
+    <footer className='social-media'>
+      <a href="https://api.whatsapp.com/send?phone=1161712268" target="_blank"><i class="bx bxl-whatsapp"></i></a>
+      <a href="https://instagram.com/bahalabeauty?igshid=MzRlODBiNWFlZA==" target="_blank"><i class="bx bxl-instagram"></i></a>
     </footer>
   )
 }
