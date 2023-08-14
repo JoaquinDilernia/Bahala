df52cb818549f39d7ac2051da9b177c0a3a058de
joacodiler <jdilernia99@gmail.com>
2023-08-13 21:19:26 -0300

    animaciobes correcion

--- src/components/Local/Local.jsx
+++ src/components/Local/Local.jsx
@@ -41,10 +41,10 @@ const Local = () => {
 
     <div className='contenedor-info'>
 
-    <motion.div initial={{ x: -300 }} whileInView={{ x: 0 }} transition={{ duration: 1 }} className='conte-img-local'>
+    <motion.div initial={{ x: -100 }} whileInView={{ x: 0 }} transition={{ duration: 1 }} className='conte-img-local'>
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3292.00435820174!2d-58.64089212336877!3d-34.401235146834566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bca73f7a5ecced%3A0x712d97d7877561d0!2sBahala%20Beauty!5e0!3m2!1ses!2sar!4v1691607958784!5m2!1ses!2sar"  loading="lazy" ></iframe>
           </motion.div>
-          <motion.div initial={{ x: 300 }} whileInView={{ x: 0 }} transition={{ duration: 1 }} className='conte-txt-local'>
+          <motion.div initial={{ x: 100 }} whileInView={{ x: 0 }} transition={{ duration: 1 }} className='conte-txt-local'>
           <p className='texto-horarios'> Martes a Domingo: de 10:00 a 19:00 hs.</p>
           <p className='texto-horarios'>  Av. Agustín M. García 6385, B1648 Rincón de Milberg, Provincia de Buenos Aires</p>
           <p className='texto-horarios'>  011 6171-2268</p>
