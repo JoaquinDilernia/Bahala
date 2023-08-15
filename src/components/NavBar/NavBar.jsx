278d7c1852a3bbe0a0e1ccb50caaab611b363c76
joacodiler <jdilernia99@gmail.com>
2023-08-15 16:12:29 -0300

    mejoras al navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,40 +1,90 @@
 import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
 import './NavBar.css'
 import logo from './logo.png'
 
 
 const NavBar = () => {
-    const [menu, setMenu] = useState(false);
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
 
     return (
 
-        <nav>
-            <Link to='/' className='logo'> <img src={logo} alt="logo" /> </Link>
-            <div className='menu' onClick={() => { setMenu(!menu) }}>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-            <ul className={menu ? "open" : ""}>
-                <li>
-                    <NavLink to='/'>Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/servicios'>Servicios</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/promos'>Promos</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/local'>Local</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/contacto'>Contacto</NavLink>
-                </li>
-
-            </ul>
-        </nav>
+        <>
+            <IconContext.Provider value={{ color: "#EEABBE" }}>
+                <nav className="navbar">
+                    <div className="navbar-container container">
+                        <Link to="/" className="logo" onClick={closeMobileMenu}>
+                            <img  src={logo} alt="loco" />
+                        </Link>
+                        <div className="menu-icon" onClick={handleClick}>
+                            {click ? <FaTimes /> : <FaBars />}
+                        </div>
+                        <ul className={click ? "nav-menu active" : "nav-menu"}>
+                            <li className="nav-item">
+                                <NavLink
+                                    to="/"
+                                    className={({ isActive }) =>
+                                        "nav-links" + (isActive ? " activated" : "")
+                                    }
+                                    onClick={closeMobileMenu}
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    to="/servicios"
+                                    className={({ isActive }) =>
+                                        "nav-links" + (isActive ? " activated" : "")
+                                    }
+                                    onClick={closeMobileMenu}
+                                >
+                                    Servicios
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    to="/promos"
+                                    className={({ isActive }) =>
+                                        "nav-links" + (isActive ? " activated" : "")
+                                    }
+                                    onClick={closeMobileMenu}
+                                >
+                                    Promos
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    to="/local"
+                                    className={({ isActive }) =>
+                                        "nav-links" + (isActive ? " activated" : "")
+                                    }
+                                    onClick={closeMobileMenu}
+                                >
+                                    Local
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink
+                                    to="/contacto"
+                                    className={({ isActive }) =>
+                                        "nav-links" + (isActive ? " activated" : "")
+                                    }
+                                    onClick={closeMobileMenu}
+                                >
+                                    Contacto
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </IconContext.Provider>
+        </>
 
     )
 }
